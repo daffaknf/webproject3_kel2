c90e71c87430070f410dffdcd5de63c8257e921a
daffaknf <99887131+daffaknf@users.noreply.github.com>
2023-01-05 21:05:57 +0700

    Update index.php

--- index.php
+++ index.php
@@ -6,7 +6,7 @@ error_reporting(0);
 session_start();
  
 if (isset($_SESSION['username'])) {
-    header("Location: index.php");
+    header("Location: dashboard.php");
 }
  
 if (isset($_POST['submit'])) {
@@ -18,7 +18,7 @@ if (isset($_POST['submit'])) {
     if ($result->num_rows > 0) {
         $row = mysqli_fetch_assoc($result);
         $_SESSION['username'] = $row['username'];
-        header("Location: index.php");
+        header("Location: dashboard.php");
     } else {
         echo "<script>alert('Email atau password Anda salah. Silahkan coba lagi!')</script>";
     }
